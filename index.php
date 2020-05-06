e936a5d208fed96f2da41b4ee1b1f3669cf1a707
2099492 <2099492@talnet.com>
2020-05-06 23:08:56 +0200

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php
-$host = '127.0.0.1:3306';
-$db   = 'netland';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+$db_Type = "mysql";
+$host = "localhost";
+$dbName = "netland";
+$userName = "root";
+$password = "";
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-   PDO::ATTR_EMULATE_PREPARES   => false,
-];
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    $status = $pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS);
-    echo $status;
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $conn = new PDO("mysql:host=$host;dbname=$dbName", $userName, $password);
+    echo "<h1> Welkom op het netland beheerderspaneel </h1>";
+} catch(PDOExeption $e) {
+    echo $e->getMessage();
 }
- ?>
+
+
+$result = $conn->query('select * from series');
+
+while($row=$result->fetch()){
+    echo "Title: ".$row['title']. ' ';
+    echo "Rating: ".$row['rating'].'<br>';
+
+}
+
+echo '<br>';
+$result = $conn->query('select * from films');
+?>
